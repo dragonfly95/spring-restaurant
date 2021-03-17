261918f66390261bf7b7b04742e1abd7be75b304
dragonfly95 <dQzNKCyAgf43e3B>
2021-03-17 22:00:00 +0900

    sdf

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -7,24 +7,8 @@ CREATE TABLE `tbl_user` (
   `user_password` varchar(45) NOT NULL,
   `user_email` varchar(45) DEFAULT NULL,
   `user_type` enum('waiter','chef','admin') NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `login_UNIQUE` (`login`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `tbl_order_detail` (
-  `order_detail_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `menu_id` int(11) NOT NULL,
-  `menu_price` float NOT NULL,
-  `menu_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`order_detail_id`),
-  KEY `order_id_idx` (`order_id`),
-  KEY `menu_id_idx` (`menu_id`),
-  CONSTRAINT `order_detail_menu` FOREIGN KEY (`menu_id`) REFERENCES `tbl_menu` (`menu_id`),
-  CONSTRAINT `order_detail_order` FOREIGN KEY (`order_id`) REFERENCES `tbl_order` (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
@@ -43,22 +27,9 @@ CREATE TABLE `tbl_order` (
   `message` varchar(255) NOT NULL,
   `total_cost` decimal(5,2) NOT NULL,
   `cooking_status` enum('Not started','Started','Canceled') NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `order_user` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
+  PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-CREATE TABLE `tbl_order` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `total_cost` decimal(5,2) NOT NULL,
-  `cooking_status` enum('Not started','Started','Canceled') NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `order_user` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB;
 
 
 CREATE TABLE `tbl_order_detail` (
@@ -68,9 +39,5 @@ CREATE TABLE `tbl_order_detail` (
   `menu_price` float NOT NULL,
   `menu_name` varchar(45) NOT NULL,
   `message` varchar(255) NOT NULL,
-  PRIMARY KEY (`order_detail_id`),
-  KEY `order_id_idx` (`order_id`),
-  KEY `menu_id_idx` (`menu_id`),
-  CONSTRAINT `order_detail_menu` FOREIGN KEY (`menu_id`) REFERENCES `tbl_menu` (`menu_id`),
-  CONSTRAINT `order_detail_order` FOREIGN KEY (`order_id`) REFERENCES `tbl_order` (`order_id`)
+  PRIMARY KEY (`order_detail_id`)
 ) ENGINE=InnoDB;
